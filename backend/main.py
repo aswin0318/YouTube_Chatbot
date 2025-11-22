39895305199bd73c50d002a3aaebd304ba58c17e
Aswin S <aswinsatheesh03@gmail.com>
2025-11-22 12:08:52 +0530

    add API Endpoint for the app (main.py)

---
+++ backend/main.py
@@ -0,0 +1,28 @@
+#API Endpoints for the application
+from fastapi import FastAPI
+from yt_bot_logic import process_request
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Chat_Request(BaseModel):
+    url: str
+    query: str
+    chat_history: list = []
+
+class Chat_Response(BaseModel):
+    answer: str
+    chat_history: list
+
+@app.post('/chat',response_model=Chat_Response)
+def chat_api(response : Chat_Request):
+    answer,updated_history = process_request(
+        url = response.url,
+        query = response.query,
+        chat_history = response.chat_history
+    )
+    return Chat_Response(answer=answer,chat_history=updated_history)
+
+@app.get('/')
+def home():
+    return {"message"  : "API is running....."}
